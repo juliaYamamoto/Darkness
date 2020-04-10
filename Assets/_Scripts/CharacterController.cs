b1415a4804983d4d39f33cc9333cb7e8626c7f7f
juliaYamamoto <juliaYamamoto@users.noreply.github.com>
2020-04-10 19:31:13 -0400

    Character movement
    Add movement using horizontal and vertical values

---
+++ Assets/_Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed;
+
+
+    void FixedUpdate()
+    {
+        horizontalInput = (Input.GetAxis("Horizontal") * speed) * Time.deltaTime;
+        verticalInput = (Input.GetAxis("Vertical") * speed) * Time.deltaTime;
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput));
+    }
+}
