459ff4314de525384b1721245063867f2ee393bc
juliaYamamoto <juliaYamamoto@users.noreply.github.com>
2020-04-11 03:53:47 -0400

    add light control for the corals

--- Assets/_Scripts/CharacterController.cs
+++ Assets/_Scripts/CharacterController.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-
+    //Movement
     private float horizontalInput;
     private float verticalInput;
     public float speed;
 
+    //Light
+    private float minLightIntensity = 5.0f;
+    private float maxLightIntensity = 20.0f;
 
-    void FixedUpdate()
+    public float currentLightIntensity = 0.0f;
+    public Light characterLight;
+
+
+    private void Start()
     {
-        horizontalInput = (Input.GetAxis("Horizontal") * speed) * Time.deltaTime;
-        verticalInput = (Input.GetAxis("Vertical") * speed) * Time.deltaTime;
+        Setup();
+    }
 
+    private void Setup()
+    {
+        //Light
+        currentLightIntensity = minLightIntensity;
+        characterLight.intensity = currentLightIntensity;
+    }
+
+
+    private void Update()
+    {
+        //Movement
         transform.Translate(new Vector3(horizontalInput, verticalInput));
     }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        horizontalInput = (Input.GetAxis("Horizontal") * speed) * Time.fixedDeltaTime;
+        verticalInput = (Input.GetAxis("Vertical") * speed) * Time.fixedDeltaTime;
+    }
+
+   
+
 }
---
+++ Assets/_Scripts/CoralController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoralController : MonoBehaviour
+{
+    //Light
+    private float minLightIntensity = 0.8f;
+    private float maxLightIntensity = 3.0f;
+
+    public bool isTouched = false;
+    public bool isGettingLight = false;
+    public float speedOnGettingLight = 2.0f;
+    public Light characterLight;
+
+
+
+    private void Start()
+    {
+        Setup();
+    }
+
+    private void Setup()
+    {
+        //Light
+        characterLight.intensity = minLightIntensity;
+    }
+
+    private void Update()
+    {
+        if (isGettingLight) {
+            characterLight.intensity += (speedOnGettingLight * Time.deltaTime);
+
+            if (characterLight.intensity >= maxLightIntensity) {
+                characterLight.intensity = maxLightIntensity;
+                isGettingLight = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Character" && !isTouched)
+        {
+            isTouched = true;
+            isGettingLight = true;
+        }
+    }
+}
