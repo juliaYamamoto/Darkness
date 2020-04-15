82b9bfa8053556e4de339dfc5fa3d2a6e76962b2
juliaYamamoto <juliaYamamoto@users.noreply.github.com>
2020-04-15 17:00:45 -0400

    Add sprites for the lightball

--- Assets/_Scripts/LightballController.cs
+++ Assets/_Scripts/LightballController.cs
@@ -4,11 +4,5 @@ using UnityEngine;
 
 public class LightballController : MonoBehaviour
 {
-    public GameObject roundball;
 
-    private void Update()
-    {
-        //Movement
-        transform.Rotate(new Vector3(0, 0, 30 * Time.deltaTime));
-    }
 }
